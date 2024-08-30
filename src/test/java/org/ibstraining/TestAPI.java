2309b6e322280845085104206ab392d8f6a92bbb
Viktor_MN <mikhaylovvn1101@gmail.com>
2024-08-30 21:18:55 +0500

    добавлен запрос Post

--- src/test/java/org/ibstraining/TestAPI.java
+++ src/test/java/org/ibstraining/TestAPI.java
@@ -1,28 +1,54 @@
 package org.ibstraining;
 
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import io.restassured.http.Cookies;
 import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.authentication;
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class TestAPI {
 
     @Test
     public void test() {
+        Response response = given()
+                .baseUri("http://localhost:8080")
+                .when()
+                .get("/api/food");
+
+        String cookie = response.getCookie("JSESSIONID");
+        System.out.println("\n\n\n" + cookie + "\n\n\n");
 
 
         given()
                 .baseUri("http://localhost:8080")
+                .sessionId(cookie)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "   \"name\": \"Кабачок\",\n" +
+                        "   \"type\": \"FRUIT\",\n" +
+                        "   \"exotic\": true \n" +
+                        "}")
+                .basePath("/api/food")
                 .when()
-                .log().all()
-                .get("/food")
+//                .log().all()
+                .post()
                 .then()
-                .body("html.body.div.div[1].div.table.tbody.tr[3].td[0]", equalTo("Яблоко"));
+                .log().all();
 
+        given()
+                .baseUri("http://localhost:8080")
+                .sessionId(cookie)
+                .log().all()
+                .when()
+                .get("/api/food")
+                .then()
+                .log().body();
 
     }
+
 }
