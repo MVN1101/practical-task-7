020b6df4cc344860e4ae6a7246e300d54815903a
Viktor_MN <mikhaylovvn1101@gmail.com>
2024-09-01 11:55:40 +0500

    добавлены проверки

---
+++ src/main/java/pojos/ProductPojo.java
@@ -0,0 +1,33 @@
+package pojos;
+
+
+public class ProductPojo {
+
+    private String name;
+    private String type;
+    private boolean exotic;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isExotic() {
+        return exotic;
+    }
+
+    public void setExotic(boolean exotic) {
+        this.exotic = exotic;
+    }
+}
--- src/test/java/org/ibstraining/TestAPI.java
+++ src/test/java/org/ibstraining/TestAPI.java
@@ -1,21 +1,22 @@
 package org.ibstraining;
 
 import io.restassured.http.ContentType;
-import io.restassured.http.Cookie;
-import io.restassured.http.Cookies;
-import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
 
+/**
+ * @author Vikor_Mikhaylov
+ * Класс для прогонки параметризированного теста корректности добавления товара с помощью API
+ */
 public class TestAPI {
 
-
+    private String sessionId;
 
     @ParameterizedTest
     @CsvSource(value = {
@@ -24,15 +25,26 @@ public class TestAPI {
             "Банан, FRUIT, false",
             "Гро-мишель, FRUIT, true"
     })
-    public void test(String fruitName, String type, Boolean isExotic) {
-        String sessionId;
+    public void addProductTest(String fruitName, String type, Boolean isExotic) {
+
+//        получение списка товаров
         Response response = given()
                 .baseUri("http://localhost:8080")
                 .when()
                 .get("/api/food");
 
+        Assertions.assertEquals(200, response.getStatusCode(), "список товаров не получен");
+
+//        проверка того, что список товаров не пустой
+        response.then()
+                .assertThat()
+                .body("", not(empty()));
+
+//        получение cookie по имени для последующей работы в рамках одной сессии
         sessionId = response.getCookie("JSESSIONID");
 
+
+//        добавление товара
         given()
                 .baseUri("http://localhost:8080")
                 .sessionId(sessionId)
@@ -40,24 +52,46 @@ public class TestAPI {
                 .body("{\n" +
                         "   \"name\":  \"" + fruitName + "\",\n" +
                         "   \"type\":   \"" + type + "\",\n" +
-                        "   \"exotic\": "+  isExotic + " \n" +
+                        "   \"exotic\": " + isExotic + " \n" +
                         "}")
                 .basePath("/api/food")
                 .when()
-                .log().all()
                 .post()
                 .then()
-                .log().all();
+                .assertThat()
+                .statusCode(200);
 
-
-        given()
+//        запрос списка после добавления товара
+        response = given()
                 .baseUri("http://localhost:8080")
                 .sessionId(sessionId)
-                .log().all()
                 .when()
-                .get("/api/food")
-                .then()
-                .log().body();
+                .get("/api/food");
+
+//        проверка наличия добавленного товара в списке
+        Assertions.assertEquals(fruitName, response.jsonPath().getString("name[4]"));
+        Assertions.assertEquals(type, response.jsonPath().getString("type[4]"));
+        Assertions.assertEquals(isExotic, response.jsonPath().getBoolean("exotic[4]"));
+
+//                .then()
+//                .assertThat()
+//                .log().all()
+//                .assertThat()
+//                .extract()
+//
+//
+//
+//                .jsonPath().getList("name")
+//                ;
+////                .jsonPath().getList("name[1]", ProductPojo.class);
+//
+//        System.out.println(list);
+
+
+//
+//
+//
+//
 
     }
 
